a55aa1343aaad046570e8ef61e10707768bd61bb
Heaten <heaten.mistry@snapwi.re>
2020-04-06 09:31:23 -0400

    [bug-fix] changes made to both twilioMessages seeders, removal of extra spaces in self-isolation record

--- DB/seeders/20200325020244-TwilioMessage.js
+++ DB/seeders/20200325020244-TwilioMessage.js
@@ -12,8 +12,8 @@ module.exports = {
       (5, 'Menu','For COVID-19 News Update Text 1.\n For COVID-19 Triage Text 2.\n For COVID-19 Self-isolation Text 3.\n For COVID-19 Prevention Tips Text 4.\n For Exit Text 5.\n','SMS','English', NOW(), NOW()),
       (6, 'NewsUpdate','As  of  March  20  8:30 PM,  311 confirmed cases  in Ontario, 271  cases  in  BC, 195  cases in  Alberta, 139  Cases  in  Quebec and  971 confirmed cases  in  Canada','Both','English', NOW(), NOW()),
       (7, 'Safety-Tips','Wash hands at the door and at regular intervals. \r Change Clothing when entering home from public space. \r Wash Coats every 1-2 Days. \r Avoid touching face, cover coughs & sneezes. \r Disinfect surfaces and utensils daily. \r Increase ventilation by opening windows or adjusting AC.\r Provide a protected space for vulnerable household members. \r Limit Non-essential travel even domestic. \r Limit eating in public spaces \r','Both','English', NOW(), NOW()),
-      (8, 'Self-Isolation','You   must   self-isolate.\r If   you   have   travelled   internationally   within   the   last   14   days. \r If   you   have   any   COVID-19   symptoms.   \r If you had   close   contact   with   a    COVID-19   confirmed   case. \r Please   do   not   go   to   an   emergency   department,   family   doctor   or   walk-in   clinic  unless  your   symptoms   worsen.   \r Don''t   go   to   any   public   places,   stay   at   home,   and   don’t   have   any   visitors','Both','English', NOW(), NOW()),
-      (9, 'Symptoms','Fever   greater   than   38°C   or   100.4°F.   Continuous   Cough.   and   Shortness   of   breath.','Both','English', NOW(), NOW()),
+      (8, 'Self-Isolation','You must self-isolate.\rIf you have travelled internationally within the last 14 days.\rIf you have any COVID-19 symptoms.\rIf you had close contact with a COVID-19 confirmed case.\rPlease do not go to an emergency department, family doctor or walk-in clinic unless your symptoms worsen.\rDon''t go to any public places, stay at home, and don’t have any visitors', 'Both','English', NOW(), NOW()),
+      (9, 'Symptoms','Fever greater than 38°C or 100.4°F. Continuous Cough. and Shortness of breath.' ,'Both','English', NOW(), NOW()),
       (10, 'Questions','Are you experiencing any of the following: severe difficulty breathing (e.g., struggling for each breath, speaking in single words), severe chest pain, having a very hard time waking up , feeling confused , lost consciousness.\n Reply  Yes  or  No','Both','English', NOW(), NOW()),
       (11, 'Evaluate-Answers','Call 911 or go to the nearest Emergency Department. Don''t use public transport, cover cough, avoid touching face, and follow other guidelines','Both','English', NOW(), NOW()),
       (12, 'Questions2','Are you experiencing any of the following:\n Short of breath at rest , Inability to lie down because of difficulty breathing , Chronic health conditions that you are having difficulty managing because of your current respiratory illness.\n Reply Yes or  No','Both','English', NOW(), NOW()),
@@ -30,7 +30,7 @@ module.exports = {
       (24, 'getHospitalDetails', 'The 3 closest hospital\\'s to you are', 'Both', 'English', NOW(),NOW()),
       (49, 'LanguageMenu', 'To Continue in English Press 1 , For French Press 2 ', 'Voice', 'English', NOW(),NOW()),
       (50, 'LanguageMenu', 'To Continue in English Text 1 , For French Text 2 ', 'Voice', 'English', NOW(),NOW()),
-      (51,'EvaluateProvider', 'Thankyou someone will call you soon', 'Both', 'English',NOW(),NOW());
+      (51, 'EvaluateProvider', 'Thankyou someone will call you soon', 'Both', 'English',NOW(),NOW());
     `)
   },
   // INSERT INTO `database_development`.`TwilioMessages` (`Name`, `Message`, `BotType`, `Language`) VALUES ('EvaluateProvider', 'Thankyou someone will call you soon', 'Both', 'English');
