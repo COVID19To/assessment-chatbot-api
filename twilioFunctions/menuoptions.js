c220ede4f9f1599a8207408a0a0c75b2c95d5f4a
Aditya Trivedi <aditya1.trivedi@paytm.com>
2020-04-09 11:28:23 -0400

    add trim

--- twilioFunctions/menuoptions.js
+++ twilioFunctions/menuoptions.js
@@ -1,7 +1,7 @@
 exports.handler = function (context, event, callback) {
   let responseObject = {}
   const memory = JSON.parse(event.Memory)
-  const options = memory.twilio.collected_data.ask_questions.answers.Menu.answer.toString().toLowerCase()
+  const options = memory.twilio.collected_data.ask_questions.answers.Menu.answer.toString().toLowerCase().trim()
 
   let redirectask = []
   switch (options) {
