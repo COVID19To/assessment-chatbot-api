a2500aa3a777eb3e3fe19b9b93c6eeedefb66358
Aditya Trivedi <aditya1.trivedi@paytm.com>
2020-04-09 11:23:48 -0400

    clean up

--- twilioFunctions/languagemenuoptions.js
+++ twilioFunctions/languagemenuoptions.js
@@ -6,9 +6,8 @@ exports.handler = function (context, event, callback) {
   const memory = JSON.parse(event.Memory)
   const options = memory.twilio.collected_data.ask_questions.answers.Language.answer.toString().toLowerCase()
 
-  const { Language } = setLanguageOptions(options)
-  console.log(Language, options, Language === 'English' && (options !== '1' || options !== 'english'))
-  if (Language === 'English' && (options !== '1' || options !== 'english')) {
+  const { selected } = setLanguageOptions(options)
+  if (selected === 'unknown') {
     const message = 'Invalid Language Selection. Default to English'
     responseObject = {
       actions: [{
