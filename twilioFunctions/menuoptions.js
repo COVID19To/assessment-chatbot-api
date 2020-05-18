edb7889da25bbe025df38e2fccd6fc74010e3eaa
windh <windH800000000@gmail.com>
2020-05-18 16:56:38 -0400

    merge to master

--- twilioFunctions/menuoptions.js
+++ twilioFunctions/menuoptions.js
@@ -29,7 +29,6 @@ exports.handler = function (context, event, callback) {
     case '6':
       redirectask = `${process.env.ASSESMENT_API}/getNearestCasesPostalCode`
       break
-    case '7':
     case 'exit':
       redirectask = `${process.env.ASSESMENT_API}/goodbye`
       break
