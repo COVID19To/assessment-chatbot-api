59124e0b2be03e28c9360a9324c69c16f218f1e0
windh <windH800000000@gmail.com>
2020-05-19 15:45:39 -0400

    added test route for insertion query in new table

--- testRoutes.js
+++ testRoutes.js
@@ -1,4 +1,14 @@
 var express = require('express')
+var mysql = require('mysql2')
+
+const connection = mysql.createConnection({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USERNAME,
+  password: process.env.MYSQL_PASS,
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT
+})
+
 var testRouter = express.Router()
 
 const { getNearestCases } = require('./lib')
@@ -19,6 +29,28 @@ testRouter.get('/nearestCasesApiRequest', async (req, res) => {
       error: 'Postal Code not provided in query string.'
     })
   }
+})
+
+testRouter.post('/insertIntoNewCasesSubscribers', (req, res) => {
+  let response = 'Cant do this!'
+  if (req.body) {
+    const { body: { postalCode, number } } = req
+
+    console.log(postalCode, number)
+
+    connection.query(`Insert Into NewCasesSubscribers (Subscriber, PostalCode, Active, createdAt, updatedAt)
+                        Values (${number}, '${postalCode}', 1, NOW(), NOW())`, function (error, results) {
+      if (error) console.log(error)
+
+      console.log(`Insert Into NewCasesSubscribers (Subscriber, PostalCode, Active, createdAt, updatedAt)
+                          Values (${number}, ${postalCode}, 1, NOW(), NOW())`)
+      response = results
+    })
+  }
+
+  res.send({
+    result: response
+  })
 })
 
 module.exports = testRouter
